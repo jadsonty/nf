73f190a740eee645b41da64c10645ee458c88a1e
Jadson José <jadsonjose0123@gmail.com>
2021-10-28 08:04:13 -0300

    Teste de banco de dados e criação automática de base de dados

--- src/main/java/com/sonner/nf/domain/Cliente.java
+++ src/main/java/com/sonner/nf/domain/Cliente.java
@@ -1,11 +1,18 @@
 package com.sonner.nf.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Cliente implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String codigo;
     private String nome;
